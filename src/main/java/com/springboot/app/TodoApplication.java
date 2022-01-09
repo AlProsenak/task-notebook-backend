0ec237eda3b88b6a358dceea16691634d3c7a489
ProsenakAljaz <prosenak.aljaz@gmail.com>
2022-01-09 21:45:05 +0100

    Migrated from springfox to springdoc openAPI v.1.6.4

--- src/main/java/com/springboot/app/TodoApplication.java
+++ src/main/java/com/springboot/app/TodoApplication.java
@@ -1,13 +1,23 @@
 package com.springboot.app;
 
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 
 @SpringBootApplication
-@EnableSwagger2
+@OpenAPIDefinition(info = @Info(
+		title = "Task Notebook",
+		description = "Demo task notebook application made with Spring Boot.",
+		version = "v0.1",
+		license = @License(
+				name = "Aljaž Prosenak - GitHub",
+				url = "https://github.com/ProsenakAljaz/task-notebook-application"
+		)
+))
 public class TodoApplication {
 
 	public static void main(String[] args) {
