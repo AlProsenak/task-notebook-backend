40c2ea197d937d057cbc1ee912d82ac86b912d91
ProsenakAljaz <prosenak.aljaz@gmail.com>
2022-01-09 19:54:02 +0100

    Added Swagger2 dependencies for basic API documentation and configured pathmatch properties for Spring Boot 2.6.2 to work

--- src/main/java/com/springboot/app/TodoApplication.java
+++ src/main/java/com/springboot/app/TodoApplication.java
@@ -3,8 +3,11 @@ package com.springboot.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class TodoApplication {
 
 	public static void main(String[] args) {
