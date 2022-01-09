6e049c17a246f8b8acbf1fbed98c1789bd40c9b8
ProsenakAljaz <prosenak.aljaz@gmail.com>
2022-01-09 16:43:35 +0100

    Created task service class and seperated bussines logic from controller

--- src/main/java/com/springboot/app/controller/TaskController.java
+++ src/main/java/com/springboot/app/controller/TaskController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.springboot.app.model.Task;
-import com.springboot.app.repository.ITaskRepository;
+import com.springboot.app.service.TaskService;
 
 
 @RestController
@@ -19,73 +19,28 @@ import com.springboot.app.repository.ITaskRepository;
 public class TaskController {
 
     @Autowired
-    ITaskRepository taskRepository;
+    TaskService taskService;
 
     // GET
     @GetMapping("/tasks")
     public ResponseEntity<List<Task>> getAllTasks() {
-        try {
-            List<Task> taskList = taskRepository.findAll();
-
-            if (taskList.isEmpty() || taskList.size() == 0) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-
-            return new ResponseEntity<>(taskList, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return taskService.getAllTasks();
     }
 
     @GetMapping("/tasks/{id}")
     public ResponseEntity<Task> getTask(@PathVariable("id") long id) {
-        try {
-            Optional<Task> task = taskRepository.findById(id);
-
-            if (task.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-
-            return new ResponseEntity<>(task.get(), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return taskService.getTask(id);
     }
 
     @GetMapping("/tasks/completed")
     public ResponseEntity<List<Task>> getAllCompletedTasks() {
-        try {
-            List<Task> taskList = taskRepository.findByCompleted(true);
-
-            if (taskList.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-            else {
-                return new ResponseEntity<>(taskList, HttpStatus.OK);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return taskService.getAllCompletedTasks();
     }
 
     // POST
     @PostMapping("/tasks")
     public ResponseEntity<Task> createTask(@RequestBody Task taskBody) {
-        try {
-            Task _task = taskRepository.save(
-                    new Task(
-                        taskBody.getTitle(),
-                        taskBody.getAccountable(),
-                        taskBody.getDeadline(),
-                        taskBody.getDescription(),
-                        false
-                    )
-            );
-
-            return new ResponseEntity<>(_task, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return taskService.createTask(taskBody);
     }
 
     // PUT
@@ -94,67 +49,22 @@ public class TaskController {
             @PathVariable("id") long id,
             @RequestBody Task taskBody
     ) {
-        try {
-            Optional<Task> task = taskRepository.findById(id);
-
-            if (task.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-
-            Task _task = task.get();
-
-            _task.setTitle(taskBody.getTitle());
-            _task.setAccountable(taskBody.getAccountable());
-            _task.setDeadline(taskBody.getDeadline());
-            _task.setDescription(taskBody.getDescription());
-            _task.setCompleted(taskBody.isCompleted());
-
-            return new ResponseEntity<>(
-                    taskRepository.save(_task),
-                    HttpStatus.OK
-            );
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return taskService.updateTask(id, taskBody);
     }
 
     // DELETE
     @DeleteMapping("/tasks")
     public ResponseEntity<HttpStatus> deleteAllTasks() {
-        try {
-            taskRepository.deleteAll();
-
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return taskService.deleteAllTasks();
     }
 
     @DeleteMapping("/tasks/{id}")
     public ResponseEntity<HttpStatus> deleteTask(@PathVariable("id") long id) {
-        try {
-            Optional<Task> task = taskRepository.findById(id);
-
-            if (task.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-
-            taskRepository.delete(task.get());
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return taskService.deleteTask(id);
     }
 
     @DeleteMapping("/tasks/completed")
     public ResponseEntity<HttpStatus> deleteAllCompletedTasks() {
-        try {
-            List<Task> taskList = taskRepository.findByCompleted(true);
-
-            taskRepository.deleteAll(taskList);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return taskService.deleteAllCompletedTasks();
     }
 }
---
+++ src/main/java/com/springboot/app/service/TaskService.java
@@ -0,0 +1,146 @@
+package com.springboot.app.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.springboot.app.model.Task;
+import com.springboot.app.repository.ITaskRepository;
+
+
+@Service
+public class TaskService {
+
+    @Autowired
+    ITaskRepository taskRepository;
+
+    // GET
+    public ResponseEntity<List<Task>> getAllTasks() {
+        try {
+            List<Task> taskList = taskRepository.findAll();
+
+            if (taskList.isEmpty() || taskList.size() == 0) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(taskList, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Task> getTask(long id) {
+        try {
+            Optional<Task> task = taskRepository.findById(id);
+
+            if (task.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            return new ResponseEntity<>(task.get(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<List<Task>> getAllCompletedTasks() {
+        try {
+            List<Task> taskList = taskRepository.findByCompleted(true);
+
+            if (taskList.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            else {
+                return new ResponseEntity<>(taskList, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // POST
+    public ResponseEntity<Task> createTask(Task taskBody) {
+        try {
+            Task _task = taskRepository.save(
+                    new Task(
+                            taskBody.getTitle(),
+                            taskBody.getAccountable(),
+                            taskBody.getDeadline(),
+                            taskBody.getDescription(),
+                            false
+                    )
+            );
+
+            return new ResponseEntity<>(_task, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // PUT
+    public ResponseEntity<Task> updateTask(long id, Task taskBody) {
+        try {
+            Optional<Task> task = taskRepository.findById(id);
+
+            if (task.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            Task _task = task.get();
+
+            _task.setTitle(taskBody.getTitle());
+            _task.setAccountable(taskBody.getAccountable());
+            _task.setDeadline(taskBody.getDeadline());
+            _task.setDescription(taskBody.getDescription());
+            _task.setCompleted(taskBody.isCompleted());
+
+            return new ResponseEntity<>(
+                    taskRepository.save(_task),
+                    HttpStatus.OK
+            );
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // DELETE
+    public ResponseEntity<HttpStatus> deleteAllTasks() {
+        try {
+            taskRepository.deleteAll();
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<HttpStatus> deleteTask(long id) {
+        try {
+            Optional<Task> task = taskRepository.findById(id);
+
+            if (task.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            taskRepository.delete(task.get());
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<HttpStatus> deleteAllCompletedTasks() {
+        try {
+            List<Task> taskList = taskRepository.findByCompleted(true);
+
+            taskRepository.deleteAll(taskList);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
