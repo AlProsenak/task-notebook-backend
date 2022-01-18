25aa58e128deac83fc5519625f8e6aacd8a4472f
ProsenakAljaz <prosenak.aljaz@gmail.com>
2022-01-18 07:44:31 +0100

    Migrated to PostgreSQL database and added small logic to DataLoader

--- src/main/java/com/springboot/app/DataLoader.java
+++ src/main/java/com/springboot/app/DataLoader.java
@@ -1,6 +1,7 @@
 package com.springboot.app;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -23,39 +24,47 @@ public class DataLoader implements ApplicationRunner {
     }
 
     public void run(ApplicationArguments args) {
-        LocalDate currentDate = LocalDate.now();
+        List<Task> taskList = taskRepository.findAll();
 
-        taskRepository.save(
-                new Task(
-                    "Create TODO application",
-                    "Alex",
-                    "Create task managing notebook application using Spring Boot framework.",
-                    LocalDate.of(2022, 1, 17),
-                    null,
-                    false
-                )
-        );
+        if (taskList.isEmpty() || taskList.size() == 0) {
+            LocalDate currentDate = LocalDate.now();
 
-        taskRepository.save(
-                new Task(
-                    "Brew some tea",
-                    "Kettle",
-                    "Stay hydrated and make sure it is black. You will need it!",
-                    LocalDate.of(2022, 1, 7),
-                    currentDate,
-                    true
-                )
-        );
+            taskRepository.save(
+                    new Task(
+                            "Create TODO application",
+                            "Alex",
+                            "Create task managing notebook application using Spring Boot framework.",
+                            LocalDate.of(2022, 1, 17),
+                            null,
+                            false
+                    )
+            );
 
-        taskRepository.save(
-                new Task(
-                        "Watch some tutorials",
-                        "YouTube",
-                        "Playback speed 1.25 is your friend ... ",
-                        LocalDate.of(2022, 1, 9),
-                        currentDate,
-                        true
-                )
-        );
+            taskRepository.save(
+                    new Task(
+                            "Brew some tea",
+                            "Kettle",
+                            "Stay hydrated and make sure it is black. You will need it!",
+                            LocalDate.of(2022, 1, 7),
+                            currentDate,
+                            true
+                    )
+            );
+
+            taskRepository.save(
+                    new Task(
+                            "Watch some tutorials",
+                            "YouTube",
+                            "Playback speed 1.5 is your friend ... ",
+                            LocalDate.of(2022, 1, 9),
+                            currentDate,
+                            true
+                    )
+            );
+
+            System.out.println("\nDatabase empty. Added mock data from DataLoader ... ");
+        } else {
+            System.out.println("\nDatabase populated. Skip adding mock data from DataLoader ... ");
+        }
     }
 }
