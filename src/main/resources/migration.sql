64e75b047f228b15f3780ad2de153ddfd833ddb0
ProsenakAljaz <prosenak.aljaz@gmail.com>
2022-01-20 08:00:20 +0100

    Added Dockerfile

--- src/main/resources/migration.sql
+++ src/main/resources/migration.sql
@@ -1,9 +1,8 @@
-
-CREATE TABLE tbl_task (
+CREATE TABLE IF NOT EXISTS tbl_task (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     accountable VARCHAR(50),
-    description VARCHAR(250) NOT NULL,
+    description VARCHAR(255) NOT NULL,
     deadline DATE,
     completion_date DATE,
     completed BOOLEAN NOT NULL
